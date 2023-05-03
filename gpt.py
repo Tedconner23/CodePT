1a914b0f736cb774540d4006a40e834bae2f1158
Ted Conner <42025319+Tedconner23@users.noreply.github.com>
2023-05-02 22:19:47 -0700

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,83 +1,109 @@
 import asyncio
-import openai
-from config import *
-from EmbeddingTools import EmbeddingsTools
-from Utils import SlidingWindowEncoder, VectorDB
+import os
+import re
+from gpt import gpt_interaction, SlidingWindowEncoder
+from redismem import clear_redis_memory, ingest_git_repo, ingest_pdf_files, print_files_in_redis_memory, get_history_from_redis, save_history_to_redis
+from config import write_response_to_file, calculate_max_tokens
 from datastructures import Planning, Task
-import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
-class GPTInteraction:
-    embeddings_tools = EmbeddingsTools(api_key)
-    vector_db = VectorDB()
-    sliding_window_encoder = SlidingWindowEncoder()
+
+async def process_redis_memory_context():
+    keys = vector_db.get_keys_by_prefix('repo:') + vector_db.get_keys_by_prefix('pdf:')
+    embeddings = [np.array(vector_db.get_item(key), dtype=float) for key in keys]
+    ada_embeddings = await get_ada_embeddings([key for key in keys])
+    return dict(zip(keys, ada_embeddings))
+
+
+async def get_memory_keywords(redis_memory_context, ada_embeddings, threshold=0.8):
+    memory_keywords = []
+    for key, value in redis_memory_context.items():
+        similarity = cosine_similarity(ada_embeddings, value)
+        if similarity >= threshold:
+            memory_keywords.append(key)
+    return memory_keywords
+
+
+async def keywordizer(planning, input_text, redis_memory_context):
+    tasks_to_execute = []
+    ada_embeddings = await get_ada_embeddings(input_text)
+    memory_keywords = await get_memory_keywords(redis_embeddings)
+
+    for task in planning.get_tasks():
+        all_keywords = task.keywords + memory_keywords
+        if any(re.search(f"\\b{keyword}\\b", input_text, re.IGNORECASE) for keyword in all_keywords):
+            tasks_to_execute.append(task)
+        else:
+            inferred_keywords = await get_similar_keywords(ada_embeddings, task.keywords)
+            if inferred_keywords:
+                tasks_to_execute.append(task)
+
+    return tasks_to_execute
+
+
+async def main():
     planning = Planning()
+    redis_memory_context = await process_redis_memory_context()
 
-    async def get_optimized_instructions(self, instruction: str, model: str = 'gpt-3.5-turbo', max_tokens: int = 100) -> str:
-        prompt = f"Create an AI understanding and compact instructions for ADAtoperform the following task optimally: {instruction}"
-        optimized_instruction = await self.gpt_interaction(prompt, model, max_tokens)
-        return optimized_instruction.strip()
+    while True:
+        display_main_menu()
+        choice = input('Enter your choice (1/2/3/4/5/6/7):')
+        
+        if choice == '1':
+            clear_redis_memory()
+            print('Redis memory cleared.')
+        elif choice == '2':
+            display_sub_menu()
+            sub_choice = input('Enter your choice (a/b):')
+            = input('Enter the directory containing PDF files:')
+                await ingest_pdf_files(directory)
+                print('PDF files ingested successfully.')
+            else:
+                print('Invalid choice. Please try again.')
+        elif choice == '3':
+            await print_files_in_redis_memory()
+        elif choice == '4':
+            conversation_history = get_history_from_redis()
+            print("Enter 'exit' to stop the GPT interaction.")
+            while True:
+                new_input = input('User:')
+                if new_input.lower() == 'exit':
+                    break
+                tasks = await keywordizer(planning, new_input, redis_memory_context)
+                for task in tasks:
+                    await planning.execute_task(task)
+                combined_input = f"{conversation_history}\nUser:{new_input}\nAI:"
+                max_tokens = calculate_max_tokens('gpt-3.5-turbo')
+                response = await gpt_interaction(combined_input, 'gpt-3.5-turbo', max_tokens)
+                conversation_history += f"User:{new_input}\nAI:{response}"
+                save_history_to_redis(conversation_history)
+                print(f"AI:{response}")
+        elif choice == '5':
+            print('Please provide the script name (script_name.py arg1 arg2 ...)')
+            list_scripts()
+            script_input = input('Enter script name and arguments:')
+            try:
+                script_args = script_input.split()
+                script_name = script_args.pop(0)
+                if script_name.endswith('.py') and os.path.exists(script_name):
+                    await asyncio.create_subprocess_exec('python', script_name, *script_args)
+                    print(f"Successfully executed {script_name} with arguments: {', '.join(script_args)}")
+                else:
+                    print(f"Script '{script_name}' not found. Please choose a valid script.")
+            except Exception as e:
+                print(f"Error executing the script: {e}")
+        elif choice == '6':
+            try:
+                await asyncio.create_subprocess_exec('python', 'main.py')
+                print('Successfully executed main.py script.')
+            except Exception as e:
+                print(f"Error executing the main.py script: {e}")
+        elif choice == '7':
+            print('Exiting...')
+            break
+        else:
+            print('Invalid choice. Please try again.')
 
-    async def gpt_interaction(self, prompt, model, max_tokens):
-        loop = asyncio.get_event_loop()
-        RESERVED_TOKENS = 50
-        CHUNK_SIZE = max_tokens - RESERVED_TOKENS
-        system_message = {'role': SYSTEM_ROLE, 'content': f"You are an {','.join(ASSISTANT_TRAITS)} assistant. Use Ada agents and other tools for GPT-3.5-turbo to aid in interactions and responses."}
-        user_message = {'role': 'user', 'content': prompt}
-        messages = [system_message, user_message]
 
-        response = await loop.run_in_executor(None, lambda: openai.ChatCompletion.create(model=model, messages=messages, max_tokens=max_tokens, n=1, stop=None, temperature=.7))
-        response_text = response['choices'][0]['message']['content']
-
-        if 'analyze with ada' in response_text.lower():
-            analysis_result = await self.embeddings_tools.get_ada_embeddings(prompt, model)
-            response_text += f"\nAda analysis result: {analysis_result}"
-
-        tasks = self.planning.keywordizer(prompt)
-        for task in tasks:
-            await self.planning.execute_task(task)
-
-        return response_text.strip()
-
-    async def search_content(self, query: str, top_k: int = 5) -> str:
-        query_embedding = await self.embeddings_tools.get_ada_embeddings(query)
-        results = self.vector_db.search(query_embedding, top_k)
-        response = f"Top {top_k} results:\n"
-        for (i, (key, similarity)) in enumerate(results):
-            response += f"{i+1}. {key}: {similarity:.2f}\n"
-        return response.strip()
-
-    async def recommend_content(self, item_key: str, top_k: int = 5) -> str:
-        results = self.vector_db.recommend(item_key, top_k)
-        response = f"Top {top_k} recommendations for {item_key}:\n"
-        for (i, (key, similarity)) in enumerate(results):
-            response += f"{i+1}. {key}: {similarity:.2f}\n"
-        return response.strip()
-
-    async def get_ada_embeddings(self, texts: list, model: str = 'text-ada-002') -> np.ndarray:
-        encoded_texts = [self.sliding_window_encoder.encode(text) for text in texts]
-        response = await loop.run_in_executor(None, lambda: openai.Embed.create(model=model, texts=encoded_texts))
-        return np.array(response['data'])
-
-    async def get_similar_texts(self, query_embedding: np.ndarray, text_embeddings: list, top_k: int = 5) -> list:
-        similarities = [self.embeddings_tools.cosine_similarity(query_embedding, text_embedding) for text_embedding in text_embeddings]
-        sorted_indices = np.argsort(similarities)[::-1]
-        return [(index, similarities[index]) for index in sorted_indices[:top_k]]
-
-    async def recommend(self, query: str, texts: list, top_k: int = 5) -> list:
-        query_embedding = await self.get_ada_embeddings([query])[0]
-        text_embeddings = await self.get_ada_embeddings(texts)
-        return await self.get_similar_texts(query_embedding, text_embeddings, top_k)
-
-    async def get_average_embedding(self, texts: list, model: str = 'text-ada-002') -> np.ndarray:
-        embeddings = await self.get_ada_embeddings(texts, model)
-        return np.mean(embeddings, axis=0)
-
-    async def get_nearest_neighbors(self, query_embedding: np.ndarray, text_embeddings: list, top_k: int = 5) -> list:
-        distances = [np.linalg.norm(query_embedding - text_embedding) for text_embedding in text_embeddings]
-        sorted_indices = np.argsort(distances)
-        return [(index, distances[index]) for index in sorted_indices[:top_k]]
-
-    async def search(self, query: str, texts: list, top_k: int = 5) -> list:
-        query_embedding = await self.get_ada_embeddings([query])[0]
-        text_embeddings = await self.get_ada_embeddings(texts)
-        return await self.get_nearest_neighbors(query_embedding, text_embeddings, top_k)
+if __name__ == '__main__':
+    asyncio.run(main())
