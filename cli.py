abf6db7c21431b8aa5c97536c044a1af2e939b64
Ted Conner <42025319+Tedconner23@users.noreply.github.com>
2023-05-02 20:13:59 -0700

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,17 +1,16 @@
 import asyncio
 from gpt import gpt_interaction, SlidingWindowEncoder
 from redismem import (
-    get_history_from_redis,
-    save_history_to_redis,
+    clear_redis_memory,
     ingest_git_repo,
     ingest_pdf_files,
-    get_pdf_library,
     print_files_in_redis_memory,
+    get_history_from_redis,
+    save_history_to_redis,
 )
 from config import write_response_to_file, calculate_max_tokens
 from datastructures import Planning, Task
 import re
-
 
 def keywordizer(planning, input_text):
     tasks_to_execute = []
@@ -23,9 +22,9 @@ def keywordizer(planning, input_text):
             tasks_to_execute.append(task)
     return tasks_to_execute
 
-
 async def main():
     planning = Planning()
+
     while True:
         print('\nOptions:')
         print('1. Clear Redis memory')
@@ -33,27 +32,42 @@ async def main():
         print('3. Print files in Redis memory')
         print('4. Continue with GPT interaction')
         print('5. Exit')
+
         choice = input('Enter your choice (1/2/3/4/5): ')
 
         if choice == '1':
-            r.flushdb()
+            clear_redis_memory()
             print('Redis memory cleared.')
+
         elif choice == '2':
-            repo_url = input('Enter the Git repository URL: ')
-            await ingest_git_repo(repo_url)
-            directory = input('Enter the directory containing PDF files: ')
-            await ingest_pdf_files(directory)
+            print('Options:')
+            print('  a. Ingest Git repository')
+            print('  b. Ingest PDF files')
+            sub_choice = input('Enter your choice (a/b): ')
+
+            if sub_choice.lower() == 'a':
+                repo_url = input('Enter the Git repository URL: ')
+                await ingest_git_repo(repo_url)
+            elif sub_choice.lower() == 'b':
+                directory = input('Enter the directory containing PDF files: ')
+                await ingest_pdf_files(directory)
+            else:
+                print('Invalid choice. Please try again.')
+
         elif choice == '3':
             await print_files_in_redis_memory()
+
         elif choice == '4':
             conversation_history = get_history_from_redis()
+
             while True:
                 new_input = input('User: ')
                 if new_input.lower() == 'exit':
                     break
+
                 tasks = keywordizer(planning, new_input)
                 for task in tasks:
-                    planning.execute_task(task)
+                    await planning.execute_task(task)
 
                 combined_input = (
                     f"{conversation_history}\nUser: {new_input}\nAI: "
@@ -62,6 +76,7 @@ async def main():
                 response = await gpt_interaction(
                     combined_input, 'gpt-3.5-turbo', max_tokens
                 )
+
                 conversation_history += f"User: {new_input}\nAI: {response}"
                 save_history_to_redis(conversation_history)
                 print(f"AI: {response}")
@@ -69,11 +84,12 @@ async def main():
                 if 'output to file' in new_input.lower():
                     file_name = input('Enter the output file name: ')
                     write_response_to_file(response, file_name)
+
         elif choice == '5':
             break
-        else:
-            print('Invalid choice. Please try again.')
 
+        else:
+            print('Invalid choice. Please again.')
 
 if __name__ == '__main__':
     asyncio.run(main())
